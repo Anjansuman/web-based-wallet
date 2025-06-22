73695323b0b6d0e918316aa99d13a0f02fa0676e
Anjan Suman <anjansuman80@gmail.com>
2025-06-22 23:45:36 +0530

    organized abstraction.

--- apps/web/src/context/HashedAtom.ts
+++ apps/web/src/context/HashedAtom.ts
@@ -1,10 +1,18 @@
-
 import { create } from "zustand";
+import type { Hashed } from "../utils/hashed";
+
 
 interface HashedAtom {
-
+    hashed: Hashed | null,
+    setHashed: (hashed: Hashed) => void
 }
 
 export const HashedAtom = create<HashedAtom>((set) => ({
-
+    hashed: null,
+    setHashed: (hashed) => {
+        if(hashed) return;
+        set({
+            hashed: hashed
+        });
+    }
 }));
--- apps/web/src/utils/hashed.ts
+++ apps/web/src/utils/hashed.ts
@@ -1,8 +1,10 @@
 import crypto from "crypto-js";
 import { usePopUp } from "../context/PopUpPanelContext";
 import type { Account, AccountType2 } from "../types/AccountType";
+import { Wallet } from "@ethereumjs/wallet";
 
-export class hashed {
+export class Hashed {
+
     private mnemonic: string = "";
 
     private salt: string;
@@ -11,61 +13,17 @@ export class hashed {
 
     private password: string = "";
 
-    private totalAccounts: number = 0;
+    private accounts: Account[] | null = null;
 
-    constructor(salt: string, iv: string) {
+    constructor(salt: string, iv: string, password?: string, key?: crypto.lib.WordArray, mnemonic?: string) {
         this.salt = salt;
         this.iv = iv;
+        this.password = password!;
+        this.key = key!;
+        this.mnemonic = mnemonic!;
     }
 
-    public setKey(key: crypto.lib.WordArray) {
-        this.key = key;
-    }
-
-    public PKBDF2(password: string) {
-        this.password = password;
-        return crypto.PBKDF2(password, this.salt, {
-            keySize: 256 / 32,
-            iterations: 100000
-        });
-    }
-
-    public encryptMnemonic(mnemonic: string, key: crypto.lib.WordArray) {
-        this.mnemonic = mnemonic;
-        return crypto.AES.encrypt(this.mnemonic, key, {
-            iv: crypto.enc.Hex.parse(this.iv)
-        });
-    }
-
-    public decryptMnemonic(cipherText: string, key: crypto.lib.WordArray) {
-        return crypto.AES.decrypt(cipherText, key.toString(), {
-            iv: crypto.enc.Hex.parse(this.iv)
-        });
-    }
-
-    public randomWordArray(length: number) {
-        return crypto.lib.WordArray.random(length);
-    }
-
-    public encryptString(str: string, key: crypto.lib.WordArray, iv: string) {
-        return crypto.AES.encrypt(str, key, {
-            iv: crypto.enc.Hex.parse(iv),
-            mode: crypto.mode.CBC,
-            padding: crypto.pad.Pkcs7
-        }).toString();
-    }
-
-    public decryptString(cipherText: string, key: crypto.lib.WordArray, iv: string) {
-        return crypto.AES.decrypt(cipherText, key, {
-            iv: crypto.enc.Hex.parse(iv),
-            mode: crypto.mode.CBC,
-            padding: crypto.pad.Pkcs7
-        }).toString(crypto.enc.Utf8);
-    }
-
-    public toHex(buffer: Uint8Array) {
-        return Array.from(buffer).map((b) => b.toString(16).padStart(2, "0")).join("");
-    }
+    //  <------------------ MANAGERS ------------------>
 
     public fetchChromeData(localName: string, password?: string) {
         switch (localName) {
@@ -78,9 +36,174 @@ export class hashed {
         }
     }
 
-    public fetchAndDecryptAccounts(): Promise<Account[]> | void {
+    //  <------------------ ENCRYPTION/DECRYPTION ------------------>
 
-        const { showPanel } = usePopUp();
+    public encryptMnemonic(mnemonic: string, key: crypto.lib.WordArray): crypto.lib.CipherParams {
+        this.mnemonic = mnemonic;
+        return crypto.AES.encrypt(this.mnemonic, key, {
+            iv: crypto.enc.Hex.parse(this.iv)
+        });
+    }
+
+    public decryptMnemonic(cipherText: string, key: crypto.lib.WordArray): crypto.lib.WordArray {
+        return crypto.AES.decrypt(cipherText, key.toString(), {
+            iv: crypto.enc.Hex.parse(this.iv)
+        });
+    }
+
+    public encryptString(str: string, key: crypto.lib.WordArray): string {
+        return crypto.AES.encrypt(str, key, {
+            iv: crypto.enc.Hex.parse(this.iv),
+            mode: crypto.mode.CBC,
+            padding: crypto.pad.Pkcs7
+        }).toString();
+    }
+
+    public decryptString(cipherText: string, key: crypto.lib.WordArray): string {
+        return crypto.AES.decrypt(cipherText, key, {
+            iv: crypto.enc.Hex.parse(this.iv),
+            mode: crypto.mode.CBC,
+            padding: crypto.pad.Pkcs7
+        }).toString(crypto.enc.Utf8);
+    }
+
+    // <------------------ TYPE CONVERSION ------------------>
+
+    public toHex(buffer: Uint8Array): string {
+        return Array.from(buffer).map((b) => b.toString(16).padStart(2, "0")).join("");
+    }
+
+    public toBytes(hex: string): Uint8Array {
+        if (hex.startsWith("0x")) hex.slice(2);
+        const bytes = new Uint8Array(hex.length / 2);
+
+        for (let i = 0; i < hex.length; i++) {
+            bytes[i / 2] = parseInt(hex.slice(i, i + 2), 16);
+        }
+        return bytes;
+    }
+
+    // <------------------ ACCOUNTS ------------------>
+
+    // todo
+    public createAccount(): void {
+        if (!this.mnemonic) {
+            this.showPanel("Unable to fetch seed phrase for creation of account", "error");
+            return;
+        }
+        // problem is ki account create to kr le but pehle fetch krna padega ki kitna account is seed se bana h kyunki wallet me imported accounts bhi rahenge
+
+    }
+
+    public importAccount(name: string, privateKey: string): void {
+        try {
+
+            const publicKey = this.getPublicKey(privateKey);
+
+            const str = JSON.stringify({
+                privateKey: privateKey.startsWith("0x") ? privateKey : ("0x" + privateKey),
+                publicKey: publicKey.startsWith("0x") ? publicKey : ("0x" + publicKey)
+            });
+
+            this.accounts?.push({
+                name: name,
+                privateKey: privateKey.startsWith("0x") ? privateKey : ("0x" + privateKey),
+                publicKey: publicKey.startsWith("0x") ? publicKey : ("0x" + publicKey)
+            });
+
+            const hashedKey = this.encryptString(str, this.key!);
+
+            const newAccount: AccountType2 = {
+                name: name,
+                account: hashedKey
+            };
+
+            chrome.storage.local.get("accounts", (data) => {
+                const accounts: AccountType2[] = data.accounts;
+
+                accounts.push(newAccount);
+
+                chrome.storage.local.set({ accounts });
+
+            });
+
+        } catch (error) {
+            this.showPanel("Unable to import account", "error");
+        }
+    }
+
+    public deleteAccount(name: string, publicKey: string): void {
+        if (!this.accounts) {
+            this.showPanel("Unable to fetch accounts", "error");
+            return;
+        }
+
+        // const accountIndex: number | undefined = this.accounts.find((acc, index): number | undefined => {
+        //     if(acc.name === name && acc.publicKey === publicKey) {
+        //         return index;
+        //     }
+        // });
+
+        let accountIndex = 0;
+
+        for (let i = 0; i < this.accounts.length; i++) {
+            if (this.accounts[i].name === name && this.accounts[i].publicKey === publicKey) {
+                accountIndex = i;
+                break;
+            }
+        }
+
+        if (!accountIndex) {
+            this.showPanel("Account doesn't exist", "error");
+            return;
+        }
+
+        // removed from the state
+        const allAccounts = this.accounts.map((acc) => {
+            if (acc.name !== name && acc.publicKey !== publicKey) return acc;
+        });
+
+        if (!allAccounts || allAccounts.length === 0 || typeof allAccounts === undefined) {
+            this.showPanel("No accounts left!", "error");
+            return;
+        }
+
+        // correct this
+        this.accounts = allAccounts as Account[];
+
+        chrome.storage.local.get("accounts", (data) => {
+            let accounts: AccountType2[] = data.accounts;
+
+            // map over this accounts and remove the deleting account
+            const updatedAccounts: (AccountType2 | undefined)[] = accounts.map((acc, index) => {
+                if (index !== accountIndex) {
+                    return acc;
+                }
+            });
+
+            if (!updatedAccounts) {
+                return;
+            }
+
+            // fix this [i think there's security issues]
+            accounts = updatedAccounts as AccountType2[];
+            chrome.storage.local.set({ accounts });
+
+        })
+
+    }
+
+    public getAccounts(): Account[] | void {
+
+        if (!this.accounts) {
+            this.showPanel("Unable to fetch accounts", "error");
+            return;
+        }
+
+        return this.accounts;
+    }
+
+    private fetchAndDecryptAccounts(): Promise<Account[]> | void {
 
         try {
 
@@ -90,7 +213,7 @@ export class hashed {
                         const accounts: AccountType2[] = data.accounts;
 
                         const decryptedAccounts = accounts.map((acc) => {
-                            const account = this.decryptString(acc.account, this.key!, this.iv);
+                            const account = this.decryptString(acc.account, this.key!);
                             return JSON.parse(account);
                         });
 
@@ -100,7 +223,7 @@ export class hashed {
                             publicKey: acc.publicKey
                         }));
 
-                        this.totalAccounts = allAccounts.length;
+                        this.accounts = allAccounts;
 
                         resolve(allAccounts);
 
@@ -111,25 +234,17 @@ export class hashed {
             });
 
         } catch (error) {
-            showPanel("Error occured while entering password", "error");
+            this.showPanel("Error occured while entering password", "error");
             return;
         }
     }
 
-    public createAccount() {
-        
-    }
+    // <------------------ MNEMONICS ------------------>
 
-    public deleteAccount() {
+    private fetchAndDecryptSeedPhrase(password: string): Promise<string> | void {
 
-    }
-
-    public fetchAndDecryptSeedPhrase(password: string): Promise<string> | void {
-
-        const { showPanel } = usePopUp();
-
-        if(this.password ! == password) {
-            showPanel("Wrong passwrd", "error");
+        if (this.password! == password) {
+            this.showPanel("Wrong passwrd", "error");
             return;
         }
 
@@ -144,6 +259,7 @@ export class hashed {
                         const key = this.PKBDF2(password);
                         const decryptedSeed = this.decryptMnemonic(ciphertext, key).toString();
 
+                        this.mnemonic = decryptedSeed;
                         resolve(decryptedSeed);
                     });
                 } catch (error) {
@@ -152,9 +268,52 @@ export class hashed {
             });
 
         } catch (error) {
-            showPanel("Failed to show seed phrase", "error");
+            this.showPanel("Failed to show seed phrase", "error");
             return;
         }
+    }
+
+    public getMnemonic(): string {
+        if(!this.mnemonic) {
+            this.showPanel("Unable to fetch seed phrase / mnemonic", "error");
+            return "";
+        }
+        return this.mnemonic;
+    }
+
+    // <------------------ ALGORITHMS ------------------>
+
+    public PKBDF2(password: string): crypto.lib.WordArray {
+        this.password = password;
+        return crypto.PBKDF2(password, this.salt, {
+            keySize: 256 / 32,
+            iterations: 100000
+        });
+    }
+
+    public randomWordArray(length: number): crypto.lib.WordArray {
+        return crypto.lib.WordArray.random(length);
+    }
+
+    public getPublicKey(privateKey: string): string {
+        const bufferedPrivateKey = this.toBytes(privateKey);
+        const wallet = Wallet.fromPrivateKey(bufferedPrivateKey);
+        const publicKey = "0x" + this.toHex(wallet.getPublicKey());
+
+        return publicKey;
+    }
+
+    // <------------------ DETAILS ------------------>
+
+    // <------------------ UTILS ------------------>
+
+    public setKey(key: crypto.lib.WordArray): void {
+        this.key = key;
+    }
+
+    public showPanel(message: string, type: "success" | "error") {
+        const { showPanel } = usePopUp();
+        showPanel(message, type);
     }
 
 }
